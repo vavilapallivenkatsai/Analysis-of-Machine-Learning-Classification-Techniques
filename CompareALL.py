a1d82daeeb8b986cf8dc4dfed48b96ffb02b7f8a
vavilapallivenkatsai <vavilapallivenkatsai@gmail.com>
2021-08-20 20:30:48 +0530

    Add files via upload

---
+++ CompareALL.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 30 01:09:57 2018
+
+@author: mapvr
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+#importing all the required ML packages
+from sklearn.linear_model import LogisticRegression #logistic regression
+from sklearn import svm #support vector Machine
+from sklearn.ensemble import RandomForestClassifier #Random Forest
+from sklearn.neighbors import KNeighborsClassifier #KNN
+from sklearn.naive_bayes import GaussianNB #Naive bayes
+from sklearn.tree import DecisionTreeClassifier #Decision Tree
+from sklearn.model_selection import train_test_split #training and testing data split
+from sklearn import metrics #accuracy measure
+from sklearn.metrics import confusion_matrix #for confusion matrix
+
+dataset = pd.read_csv(r"heart.csv")
+x = dataset.iloc[:,0:13].values
+y = dataset.iloc[:, 13].values
+
+for index, item in enumerate(y):   # Last row gives 4 diff types of output , so convert them to 0  or 1 
+	if not (item == 0.0):       # that is either Yes or No
+		y[index] = 1
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+onehotencoder = OneHotEncoder(categorical_features = [2,6,10,12])
+x = onehotencoder.fit_transform(x).toarray()
+x = x[:, 1:]
+x = np.delete(x,3,1)
+x = np.delete(x,5,1)
+x = np.delete(x,7,1)
+
+from sklearn.model_selection import KFold #for K-fold cross validation
+from sklearn.model_selection import cross_val_score #score evaluation
+from sklearn.model_selection import cross_val_predict #prediction
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+xyz=[]
+accuracy=[]
+std=[]
+classifiers=['Linear Svm','Radial Svm','Logistic Regression','KNN','Decision Tree','Naive Bayes','Random Forest']
+models=[svm.SVC(kernel='linear'),svm.SVC(kernel='rbf'),LogisticRegression(),KNeighborsClassifier(n_neighbors=9),DecisionTreeClassifier(),GaussianNB(),RandomForestClassifier(n_estimators=100)]
+for i in models:
+    model = i
+    cv_result = cross_val_score(model,x,y, cv = kfold,scoring = "accuracy")
+    cv_result=cv_result
+    xyz.append(cv_result.mean())
+    std.append(cv_result.std())
+    accuracy.append(cv_result)
+new_models_dataframe2=pd.DataFrame({'CV Mean':xyz,'Std':std},index=classifiers)       
+new_models_dataframe2
+
+plt.subplots(figsize=(12,6))
+box=pd.DataFrame(accuracy,index=[classifiers])
+box.T.boxplot()
+
+new_models_dataframe2['CV Mean'].plot.barh(width=0.8)
+plt.title('Average CV Mean Accuracy')
+fig=plt.gcf()
+fig.set_size_inches(8,5)
+plt.show()
+
+
+
+#CONFUSION MATRIX
+f,ax=plt.subplots(3,3,figsize=(12,10))
+y_pred = cross_val_predict(svm.SVC(kernel='rbf'),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[0,0],annot=True,fmt='2.0f')
+ax[0,0].set_title('Matrix for rbf-SVM')
+y_pred = cross_val_predict(svm.SVC(kernel='linear'),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[0,1],annot=True,fmt='2.0f')
+ax[0,1].set_title('Matrix for Linear-SVM')
+y_pred = cross_val_predict(KNeighborsClassifier(n_neighbors=9),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[0,2],annot=True,fmt='2.0f')
+ax[0,2].set_title('Matrix for KNN')
+y_pred = cross_val_predict(RandomForestClassifier(n_estimators=100),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[1,0],annot=True,fmt='2.0f')
+ax[1,0].set_title('Matrix for Random-Forests')
+y_pred = cross_val_predict(LogisticRegression(),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[1,1],annot=True,fmt='2.0f')
+ax[1,1].set_title('Matrix for Logistic Regression')
+y_pred = cross_val_predict(DecisionTreeClassifier(),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[1,2],annot=True,fmt='2.0f')
+ax[1,2].set_title('Matrix for Decision Tree')
+y_pred = cross_val_predict(GaussianNB(),x,y,cv=10)
+sns.heatmap(confusion_matrix(y,y_pred),ax=ax[2,0],annot=True,fmt='2.0f')
+ax[2,0].set_title('Matrix for Naive Bayes')
+plt.subplots_adjust(hspace=0.2,wspace=0.2)
+plt.show()
